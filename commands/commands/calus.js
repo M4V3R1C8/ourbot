a05cbc257c509b9990c45e6506ed5f83c39a437a
M4V3R1C8 <M4v3r1c8@gmail.com>
2020-05-03 16:50:56 -0400

    Add files via upload

--- commands/commands/calus.js
+++ commands/commands/calus.js
@@ -1,15 +1,15 @@
 const Discord = require( 'discord.js' );
 module.exports = {
   name: "calus",
-  description: "Allows text callouts for calus void room. Everyone can callout at the same time with 0 audio issues. Give it a shot if voice chat is messing up.",
+  description: "Allows text callouts for Calus\' Mind\'s eye. Everyone can callout at the same time with 0 audio issues. Give it a shot if voice chat is messing up.",
   usage: "will explain how to use the 4 callouts.",
   run: async ( bot, message, args ) => {
     const Embed = new Discord.MessageEmbed()
       .setTitle( `Text Callouts for Calus.` )
       .setThumbnail( 'https://i.imgur.com/h8urTrG.png' )
       .addFields(
-        { name: `\u200b`, value: `Allows text callouts for calus void room. Everyone can callout at the same time with 0 audio issues. Give it a shot if voice chat is messing up.` },
-        { name: `\u200b`, value: 'Simply type a, c, d, or s as a single letter by itself and hit enter. The discord bot will respond via text and say which counselor to melee!' },
+        { name: `\u200b`, value: `Allows text callouts for Calus\' Mind\'s eye. Everyone can callout at the same time with 0 audio issues. Give it a shot if voice chat is messing up.` },
+        { name: `\u200b`, value: 'Simply type a, c, d, or s as a single letter by itself and hit enter. Ourbot will respond via text and say which counselor to melee!' },
       )
       .setFooter( 'I developed this functionality for the Deaf Destiny Network when the Leviathan was first released!' )
       .setColor( `0x46789d` );
