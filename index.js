5c26b3e7196c83b3e713e07c1ce13f63f0cf460c
M4V3R1C8 <M4v3r1c8@gmail.com>
2020-05-03 16:06:38 -0400

    support for the calus encounter in the leviathan raid

--- index.js
+++ index.js
@@ -14,6 +14,27 @@ bot.on( 'ready', () => {
   require( './events/client/ready' )( bot );
 } );
 bot.on( 'message', async message => {
+  // calus
+  var dupea = 0;
+  var dupec = 0;
+  var duped = 0;
+  var dupes = 0;
+  var calla = [ 'c', 'd', 's' ];
+  var callc = [ 'a', 'd', 's' ];
+  var calld = [ 'a', 'c', 's' ];
+  var calls = [ 'a', 'c', 'd' ];
+  var actual = [];
+  if ( message.content.toLowerCase() === "a" && dupea == 0 ) { dupea = "a"; actual.unshift( msg.content ); console.log( actual ); }
+  if ( message.content.toLowerCase() === "c" && dupec == 0 ) { dupec = "a"; actual.unshift( msg.content ); console.log( actual ); }
+  if ( message.content.toLowerCase() === "d" && duped == 0 ) { duped = "a"; actual.unshift( msg.content ); console.log( actual ); }
+  if ( message.content.toLowerCase() === "s" && dupes == 0 ) { dupes = "a"; actual.unshift( msg.content ); console.log( actual ); }
+  actuals = actual.sort();
+  if ( actuals.join( '|' ) === calla.join( '|' ) ) { message.channel.send( `Melee Axe! https://imgur.com/agVmO4o ` ).then( actual.length = 0 ).then( dupea = 0 ).then( dupec = 0 ).then( duped = 0 ).then( dupes = 0 ); }
+  if ( actuals.join( '|' ) === callc.join( '|' ) ) { message.channel.send( `Melee Cup! https://imgur.com/0NOS43W ` ).then( actual.length = 0 ).then( dupea = 0 ).then( dupec = 0 ).then( duped = 0 ).then( dupes = 0 ); }
+  if ( actuals.join( '|' ) === calld.join( '|' ) ) { message.channel.send( `Melee Dog! https://imgur.com/pFHvr1c ` ).then( actual.length = 0 ).then( dupea = 0 ).then( dupec = 0 ).then( duped = 0 ).then( dupes = 0 ); }
+  if ( actuals.join( '|' ) === calls.join( '|' ) ) { message.channel.send( `Melee Sun! https://imgur.com/bB4euwu ` ).then( actual.length = 0 ).then( dupea = 0 ).then( dupec = 0 ).then( duped = 0 ).then( dupes = 0 ); }
+  if ( actuals.join( '|' ) !== calla.join( '|' ) !== callc.join( '|' ) !== calld.join( '|' ) !== calls.join( '|' ) && actuals.length === 3 ) { console.log( actuals ); message.channel.send( `@M4V3R1C8#9851 There is no call out for the provided inputs, please enter again...` ).then( actual.length = 0 ); }
+  // rest of the bot
   message.member; //-- GuildMember based
   message.author; //-- User based
   require( './events/guild/message' )( bot, message );
