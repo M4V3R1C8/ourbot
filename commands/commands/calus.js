7dab3ce85f67c3ed42de0462a5ce2aa3d4b4fc17
M4V3R1C8 <M4v3r1c8@gmail.com>
2020-05-03 16:44:22 -0400

    Add files via upload

--- commands/commands/calus.js
+++ commands/commands/calus.js
@@ -5,8 +5,8 @@ module.exports = {
   usage: "will explain how to use the 4 callouts.",
   run: async ( bot, message, args ) => {
     const Embed = new Discord.MessageEmbed()
-      .setTitle( `Please look at your coorresponding position.` )
-      .setThumbnail( 'https://i.imgur.com/o6pudX7.jpg' )
+      .setTitle( `Text Callouts for Calus.` )
+      .setThumbnail( 'https://i.imgur.com/h8urTrG.png' )
       .addFields(
         { name: `\u200b`, value: `Allows text callouts for calus void room. Everyone can callout at the same time with 0 audio issues. Give it a shot if voice chat is messing up.` },
         { name: `\u200b`, value: 'Simply type a, c, d, or s as a single letter by itself and hit enter. The discord bot will respond via text and say which counselor to melee!' },
