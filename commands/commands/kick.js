4994f3e013dc52921bccd5b63e33e5082a916f76
M4V3R1C8 <M4v3r1c8@gmail.com>
2020-05-03 20:41:52 -0400

    Add files via upload

--- commands/commands/kick.js
+++ commands/commands/kick.js
@@ -6,14 +6,14 @@ module.exports = {
   usage: "<user id> <reason>",
   run: async ( bot, message, args ) => {
     var count = 0;
-    guild.members.cache.forEach( member => {
+    message.guild.members.cache.forEach( member => {
       if ( member.roles.some( "lfg" ) ) {
         count++;
         member.kick();
       }
     } );
     const Embed = new MessageEmbed()
-      .setTitle( `${ count } LFG Players have been kicked!!` )
+      .setTitle( `${ count } LFG Player(s) have been kicked!!` )
       .setColor( `0x46789d` );
     message.channel.send( Embed );
   }
