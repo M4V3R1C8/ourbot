c7e6c63d7812a9c0042a46828203bc15c0c87df5
M4V3R1C8 <M4v3r1c8@gmail.com>
2020-05-03 20:44:22 -0400

    Add files via upload

--- commands/commands/kick.js
+++ commands/commands/kick.js
@@ -7,7 +7,7 @@ module.exports = {
   run: async ( bot, message, args ) => {
     var count = 0;
     message.guild.members.cache.forEach( member => {
-      if ( member.roles.some( "lfg" ) ) {
+      if ( member.roles.has( role => role.name === "lfg" ) ) {
         count++;
         member.kick();
       }
