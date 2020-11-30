6a83b9572ffd92c678e127cdcc222a44580c6520
M4V3R1C8 <M4v3r1c8@gmail.com>
2020-11-29 23:30:05 -0500

    Add files via upload

--- first-message.js
+++ first-message.js
@@ -1,5 +1,6 @@
 const addReactions = (message, reactions) => {
   message.react(reactions[0])
+  console.log(reactions[0]);
   reactions.shift()
   if (reactions.length > 0) {
     setTimeout(() => addReactions(message, reactions), 750)
