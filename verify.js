613764cfe792f0553ed2ca45f3ebaa58d59fb940
M4V3R1C8 <M4v3r1c8@gmail.com>
2020-12-03 20:02:01 -0500

    Add files via upload

--- verify.js
+++ verify.js
@@ -11,7 +11,7 @@ module.exports = (bot) => {
 
   const reactions = []
 
-  let emojiText = '**Please click the :white_check_mark: below to agree to the rules above and to gain access to the rest of this server:**\n\n'
+  let emojiText = '\n**Please click the :white_check_mark: below to agree to the rules above and to gain access to the rest of this server:**\n\n'
   for (const key in emojis) {
     reactions.push(key)
     emojiText += ``
@@ -20,34 +20,37 @@ module.exports = (bot) => {
   firstMessage(bot, channelId, emojiText, reactions)
 
   const handleReaction = (reaction, user, add) => {
-    if(reaction.message.channel.id !== channelId) return
-    if(user.id === '704022988722274304') return
-    const { guild } = reaction.message
-    const emoji = reaction._emoji.name
-    const roleName = emojis[emoji]
-    if (!roleName) {
-      return
-    }
-    const role = guild.roles.find((role) => `${role.name}` === roleName)
-    const role2 = guild.roles.find((role2) => `Stardust` === roleName)
-    const member = guild.members.cache.find((member) => member.id === user.id)
-    if (add) {
-      console.log(`Adding role: ${role}, removing role: ${role2}`);
-      member.roles.cache.add(role)
-      member.roles.cache.remove(role2);
-    } else {
-      console.log(`Removing role: ${role}, adding role: ${role2}`);
-      member.roles.cache.remove(role)
-      member.roles.cache.add(role2);
+    if(reaction.message.channel.id === channelId) {
+      if(user.id !== '704022988722274304') {
+        const { guild } = reaction.message
+        const emoji = reaction._emoji.name
+        const roleName = emojis[emoji]
+        if (!roleName) {
+          return
+        }
+        const role = guild.roles.find((role) => `${role.name}` === roleName)
+        const role2 = guild.roles.find((role2) => `Stardust` === roleName)
+        console.log(role2);
+        const member = guild.members.cache.find((member) => member.id === user.id)
+        if (add) {
+          console.log(`Adding role: ${role}, removing role: ${role2}`);
+          member.roles.cache.add(role)
+          member.roles.cache.remove(role2);
+        } else {
+          console.log(`Removing role: ${role}, adding role: ${role2}`);
+          member.roles.cache.remove(role)
+          member.roles.cache.add(role2);
+        }
+      }
     }
   }
 
-  bot.on('messageReactionAdd', (reaction, user) => { 
+  bot.on('messageReactionAdd', (reaction, user) => {
+    console.log(`messageReactionAdded`); 
     handleReaction(reaction, user, true) 
-    console.log(`messageReactionAdded`);
   })
   bot.on('messageReactionRemove', (reaction, user) => { 
-    handleReaction(reaction, user, false) 
     console.log(`messageReactionRemoved`);
+    handleReaction(reaction, user, false) 
   })
 }
