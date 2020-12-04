8740a355693ff8400a4f6205e2695f679090a882
M4V3R1C8 <M4v3r1c8@gmail.com>
2020-12-04 00:36:45 -0500

    Add files via upload

--- handle-reaction.js
+++ handle-reaction.js
@@ -17,7 +17,7 @@ module.exports = (bot, reaction, user, add) => {
   const emoji = reaction._emoji.name
   const roleName = emojis[emoji]
   const role = reaction.message.guild.roles.cache.find((role) => `${role.name}` === roleName)
-  const member = reaction.message.members.cache.find((member) => member.id === user.id)
+  const member = reaction.message.guild.members.cache.find((member) => member.id === user.id)
   if (!roleName) return
   
   if(reaction.message.channel.id === channelId) {
