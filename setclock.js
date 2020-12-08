f8dd87989d90d59903f70f871369373a6ed9bcdf
M4V3R1C8 <M4v3r1c8@gmail.com>
2020-12-08 00:51:25 -0500

    Add files via upload

---
+++ setclock.js
@@ -0,0 +1,33 @@
+const { MessageEmbed } = require( "discord.js" );
+const moment = require('moment');
+const tz = require('moment-timezone');
+module.exports = {
+  name: "setclock",
+  aliases: [ "" ],
+  description: "Setup a server clock for your discord.",
+  usage: "<# tz database name>",
+  run: async ( bot, message, args, db ) => {
+    db.collection('guilds').doc(message.guild.id).get().then((q) => {
+      if(q.exists) {
+        prefix = q.data().prefix;
+      }
+    }).then( async () => {
+      let clockID = args[0];
+      let timezone = args[1];
+      db.collection('guilds').doc(message.guild.id).update({
+        'clockID' : clockID,
+        'timezone' : timezone
+      }).then( async () => {
+        const timeNow = moment(new Date).tz(timezone).format('hh:mm A (z)');
+        const clockChannel = bot.channels.cache.get(clockID);
+
+        clockChannel.edit({ name: `${timeNow}` }, 'Clock update').catch(console.error);
+        setInterval( () => {
+          const timeNowUpdate = moment(new Date).tz(timezone).format('hh:mm A (z)');
+          clockChannel.edit({ name: `${timeNowUpdate}` }, 'Clock update')
+            .catch(console.error);
+        }, 60000);
+      });
+    });
+  }
+}
