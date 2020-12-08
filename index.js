6c3a764572a5bae72cd89ebed61aa0224c4f6132
M4V3R1C8 <M4v3r1c8@gmail.com>
2020-12-08 00:33:32 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Discord = require( "discord.js" );
 const fs = require( "fs" );
-require('dotenv/config');
 
 //initialize bot
 const bot = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
