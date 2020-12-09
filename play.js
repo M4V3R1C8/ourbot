516c709e184a208cd05867a2623fbaf18f0deecb
M4V3R1C8 <M4v3r1c8@gmail.com>
2020-12-09 18:57:15 -0500

    Add files via upload

--- play.js
+++ play.js
@@ -1,22 +1,67 @@
 const ytdl = require('ytdl-core');
+var queue = new Map();
 module.exports = {
   name: "play",
   aliases: [ "music" ],
   description: "",
   usage: "link",
   run: async ( bot, message, args, db ) => {
-    if(!args[0]) return message.channel.send("Don't forget to include a youtube link!");
-    if(!ytdl.validateURL(args[0])) return message.channel.send("Don't forget to include a youtube link!");
-    if(!message.member.voice.channel) return message.channel.send("Join a voice channel first!");
-    const connection = await message.member.voice.channel.join();
-    var url = args[0];
-    let stream = ytdl(url, {
-        filter: "audioonly",
-        opusEncoded: true,
-        encoderArgs: ['-af', 'bass=g=10,dynaudnorm=f=200']
-    });
-    const dispatcher = connection.play(stream, { type: "opus" });
-    dispatcher.on('finish', () => {message.guild.me.voice.channel.leave()});
-    dispatcher.on('error', () => {message.channel.send("Only youtube links work for now!")});
+    if(!args[0]) return;
+    let url = args.join(" ");
+    if(!url.match(/(youtube.com|youtu.be)\/(watch)?(\?v=)?(\S+)?/)) return message.channel.send("Please provide a valid Youtube link!");
+    let serverQueue = queue.get(message.guild.id);
+    let vc = message.member.voice;
+    if(!vc) return message.channel.send("You are not in a voice channel!");
+    if(!vc.channel.permissionsFor(client.user).has('CONNECT') || !vc.channel.permissionsFor(client.user).has('SPEAK')) return message.channel.send("I do not have permission!");
+    let songinfo = await ytdl.getInfo(url);
+    let song = {
+        title: songinfo.title,
+        url: songinfo.video_url
+    }
+
+    if(!serverQueue) {
+        let queueConst = {
+            textChannel: message.channel,
+            voiceChannel: vc.channel,
+            connection: null,
+            songs: [],
+            volume: 5,
+            playing: true
+        };
+        queue.set(message.guild.id, queueConst);
+        queueConst.songs.push(song);
+        try {
+            let connection = await vc.channel.join();
+            queueConst.connection = connection
+            playSong(message.guild, queueConst.songs[0])
+        } catch (error) {
+            console.log(error);
+            queue.delete(message.guild.id);
+            return message.channel.send("There was an error playing the song! Error: " + error);
+        }
+    } else {
+        serverQueue.songs.push(song);
+        return message.channel.send(`${song.title} has been added to the queue!`)
+    }
   }
+}
+
+async function playSong(guild, song) {
+    let serverQueue = queue.get(guild.id);
+
+    if(!song){
+        serverQueue.voiceChannel.leave();
+        queue.delete(guild.id);
+        return;
+    }
+
+    const dispatcher = serverQueue.connection.play(ytdl(song.url)).on('end', () => {
+        serverQueue.songs.shift();
+        playSong(guild, serverQueue.songs[0]);
+    })
+    .on('error', () => {
+        console.log(error)
+    })
+
+    dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
 }
