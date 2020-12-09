530ddd4e9ad448b2624126e340ef30d23b1bcaf9
M4V3R1C8 <M4v3r1c8@gmail.com>
2020-12-09 18:36:35 -0500

    Add files via upload

--- commands/commands/play.js
+++ commands/commands/play.js
@@ -10,7 +10,7 @@ module.exports = {
     if(!message.member.voice.channel) return message.channel.send("Join a voice channel first!");
     const connection = await message.member.voice.channel.join();
     var url = args[0];
-    const dispatcher = connection.play(await ytdl(url), { type: 'opus' });
+    const dispatcher = connection.play(await ytdl(url), { type: 'opus', opusEncoded: true, });
     dispatcher.on('finish', () => {message.guild.me.voice.channel.leave()});
     dispatcher.on('error', () => {message.channel.send("Only youtube links work for now!")});
   }
